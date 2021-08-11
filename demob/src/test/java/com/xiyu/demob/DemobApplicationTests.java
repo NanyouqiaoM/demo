5783c207fc693a4fdb253e13da1e8389a2b1a131
caowu@maxima.onaliyun.com <caowu@maxima.onaliyun.com>
2021-08-11 11:04:32 +0800

    build

---
+++ demob/src/test/java/com/xiyu/demob/DemobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiyu.demob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemobApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
